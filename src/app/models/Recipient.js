223bcdd9e6741f498a9ffb4204825c81852f5097
HenriqueMantovani <henriquecunha44@yahoo.com.br>
2020-02-03 23:04:07 -0300

    feat: Criação de Destinatários

--- src/app/models/Recipient.js
+++ src/app/models/Recipient.js
@@ -4,16 +4,17 @@ class Recipient extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        rua: Sequelize.STRING,
-        numero: Sequelize.STRING,
-        complemento: Sequelize.STRING,
-        estado: Sequelize.STRING,
-        cidade: Sequelize.STRING,
-        cep: Sequelize.STRING,
+        name: Sequelize.STRING,
+        street: Sequelize.STRING,
+        number: Sequelize.STRING,
+        complement: Sequelize.STRING,
+        state: Sequelize.STRING,
+        city: Sequelize.STRING,
+        zip_code: Sequelize.STRING,
       },
       {
         sequelize,
+        tableName: 'recipient',
       }
     );
     return this;
