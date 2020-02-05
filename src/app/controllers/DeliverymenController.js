87128d0942fa4dbcfea45a7b992dbac494d3e7f2
HenriqueMantovani <henriquecunha44@yahoo.com.br>
2020-02-04 23:34:29 -0300

    feat: Enpoint de Criação de entregador

--- src/app/controllers/DeliverymenController.js
+++ src/app/controllers/DeliverymenController.js
@@ -1,46 +1,50 @@
 import * as Yup from 'yup';
 import DeliveryMen from '../models/DevileryMen';
 
+// Regex para validação de email
+function validateEmail(email) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
 class RecipientController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      avatar_id: Yup.string().required(),
-      number: Yup.string().required(),
-      complement: Yup.string().required(),
-      state: Yup.string().required(),
-      city: Yup.string().required(),
-      zip_code: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
     });
 
     // Ver se o req.body esta passando igual ao schema
 
     if (req.body.name === '')
-      return res.status(400).json({ Message: 'Type the name' });
-    if (req.body.street === '')
-      return res.status(400).json({ Message: 'Type the street' });
-    if (req.body.number === '')
-      return res.status(400).json({ Message: 'Type the number' });
-    if (req.body.complement === '')
-      return res.status(400).json({ Message: 'Type the complement' });
-    if (req.body.state === '')
-      return res.status(400).json({ Message: 'Type the state' });
-    if (req.body.city === '')
-      return res.status(400).json({ Message: 'Type the city' });
-    if (req.body.zip_code === '')
-      return res.status(400).json({ Message: 'Type the zip code' });
+      return res.status(400).json({ Error: 'Type the name' });
+    if (req.body.email === '')
+      return res.status(400).json({ Error: 'Type the email' });
+    if (req.body.email && !validateEmail(req.body.email))
+      return res.status(400).json({ Error: 'Email inserido Incorretamente' });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ Error: 'Validation Fails' });
     }
 
-    const recipient = await Recipient.create(req.body);
+    // Procura pelo "email" se ja existe um usuário cadastrado
+    const deliveryExists = await DeliveryMen.findOne({
+      where: { email: req.body.email },
+    });
 
-    return res.json(recipient);
+    if (deliveryExists) {
+      return res.status(400).json({ Error: 'User alredy exists.' });
+    }
+
+    const delivery = await DeliveryMen.create(req.body);
+
+    return res.json(delivery);
   }
 
   // TODO: Fazer logica no name e newName e terminar o update
-  async update(req, res) {
+  /* async update(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       newName: Yup.string(),
@@ -77,7 +81,7 @@ class RecipientController {
     const recipientUpdated = await recipient.update(body);
 
     return res.json(recipientUpdated);
-  }
+  } */
 }
 
 export default new RecipientController();
